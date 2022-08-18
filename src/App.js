bf7691f86998b5c81fd3c2076e0976a21978d9d5
ChanCheng KU <ccc908925@gmail.com>
2022-08-18 14:47:54 +0800

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import './App.css';
 import {
   HashRouter,
   NavLink,
+  Routes,
+  Route,
 } from 'react-router-dom';
+
+const Home = () => {
+  return <h1>Home Page</h1>;
+}
+
+const Register = () => {
+  return <p>這是註冊頁面</p>;
+};
+
+const Login = () => {
+  return <p>這是登入頁面</p>;
+};
 
 const Todo = () => {
   return <p>這是 Todo 頁面 
   </p>;
 };
-const Login = () => {
-  return <p>這是登入頁面</p>;
-};
-const Register = () => {
-  return <p>這是註冊頁面</p>;
-};
+
+const NotFoundPage = () => {
+  return <h1>Not Found Page！</h1>
+}
 
 function App() {
   return (
@@ -34,6 +46,13 @@ function App() {
           </NavLink>
         </div>
         {/* Routes, Route 練習區 */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/todo" element={<Todo />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
         {/* 練習區 */}
       </HashRouter>
     </div>
